c6686f22b40f128c98f0942957edff9a2502a2d2
Isaul <tiradolisaul@gmail.com>
2025-01-04 15:09:36 -0700

    testing add strings implemented

--- pytest-fcc/tests/test_my_functions.py
+++ pytest-fcc/tests/test_my_functions.py
@@ -5,3 +5,16 @@ def test_add():
 	result = my_functions.add(1, 3)
 	assert result == 4
 
+
+def test_add_strings():
+	result = my_functions.add('say my ', 'name')
+	assert result == 'say my name'
+
+def test_divide(): 
+	result = my_functions.divide(10, 2)
+	assert result == 5
+
+
+def test_divide_by_zero(): 
+	with pytest.raises(ValueError): 
+		my_functions.divide(10, 0)
