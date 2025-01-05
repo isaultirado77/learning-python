167120dbe4bdd37602e36394dda16b4517014e57
Isaul <tiradolisaul@gmail.com>
2025-01-05 15:41:21 -0700

    shapes & circle class implemented

--- pytest-fcc/source/shapes.py
+++ pytest-fcc/source/shapes.py
@@ -1,3 +1,22 @@
-class Shape: 
-	pass
-	
+import math
+from abc import ABC, abstractmethod
+
+class Shape(ABC): 
+    @abstractmethod
+    def area(self) -> float: 
+        pass
+
+    @abstractmethod
+    def perimeter(self) -> float: 
+        pass
+
+class Circle(Shape):
+    def __init__(self, r: float):
+        self.radius = r
+
+    def area(self): 
+        return math.pi * self.radius**2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
