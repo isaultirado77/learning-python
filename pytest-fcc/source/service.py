bbd2dc25c86c9139972cd359b464c0db7173ab54
Isaul <tiradolisaul@gmail.com>
2025-01-10 21:20:59 -0700

    testing using requests

--- pytest-fcc/source/service.py
+++ pytest-fcc/source/service.py
@@ -1,3 +1,6 @@
+import requests
+
+
 database = {
 	1: 'Alice', 
 	2: 'Bob',
@@ -7,3 +10,10 @@ database = {
 
 def get_user_from_db(id: int): 
 	return database.get(id)
+
+def get_users(): 
+	response = requests.get("https://jsonplaceholder.typicode.com/users")
+	if response.status_code == 200: 
+		return response.json()
+	raise requests.HTTPError
+
